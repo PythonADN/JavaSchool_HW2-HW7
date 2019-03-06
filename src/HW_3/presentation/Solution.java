4b3a4547503acc8dbc06aebcb5f100f14af83e28
ADN <1434-3541@mail.ru>
2019-03-06 17:56:48 +0300

    Commit HW3

--- src/HW_3/presentation/Solution.java
+++ src/HW_3/presentation/Solution.java
@@ -1,5 +1,35 @@
 package HW_3.presentation;
 
-public class Solution {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String fileName = "src\\HW_3\\presentation\\file.txt";
+        System.out.println(solution.uniqueWordCount(fileName));
+
+    }
+
+    /**
+     * Задание 1: Подсчитайте количество различных слов в файле.
+     */
+    public int uniqueWordCount(String fileName) {
+        Set<String> set = new HashSet<>();
+        try {
+            Scanner scanner = new Scanner(new FileReader(fileName));
+            while(scanner.hasNext()) {
+                set.add(scanner.next().toLowerCase()); // без учёта регистра
+            }
+            scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return set.size();
+    }
 }
