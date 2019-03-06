fbb4d04126e8b66fb0363b2bea367d5d008e9ee7
ADN <1434-3541@mail.ru>
2019-03-06 19:35:32 +0300

    Commit HW3

--- src/HW_3/presentation/Solution.java
+++ src/HW_3/presentation/Solution.java
@@ -10,10 +10,14 @@ public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
         String fileName = "src\\HW_3\\presentation\\file.txt";
+        System.out.println("\nЗадание 1");
         System.out.println(solution.uniqueWordCount(fileName)); // задание 1
+        System.out.println("\nЗадание 2");
         solution.listUniqueWords(fileName); // задание 2
-        // задание 3
-
+        System.out.println("\nЗадание 3");
+        solution.wordCounterList(fileName); // задание 3
+        System.out.println("\nЗадание 4");
+        solution.reverseLine(fileName); // задание 4
     }
 
     /**
@@ -21,12 +25,12 @@ public class Solution {
      */
     public int uniqueWordCount(String fileName) {
         Set<String> set = new HashSet<>();
-        try {
-            Scanner scanner = new Scanner(new FileReader(fileName));
-            while (scanner.hasNext()) {
-                set.add(scanner.next().toLowerCase()); // без учёта регистра
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            while (reader.ready()) {
+                String line = reader.readLine();
+                String[] str = line.trim().toLowerCase().split("[\\p{IsPunctuation}\\p{IsWhite_Space}\\n]+");
+                set.addAll(Arrays.asList(str));
             }
-            scanner.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -37,7 +41,7 @@ public class Solution {
      * Задание 2: Выведите на экран список различных слов файла, отсортированный по возрастанию их длины (компаратор сначала по длине слова, потом по тексту).
      */
     public void listUniqueWords(String fileName) {
-        Set set = new TreeSet<>(
+        Set<String> set = new TreeSet<>(
                 new Comparator<String>() {
                     @Override
                     public int compare(String o1, String o2) {
@@ -47,12 +51,12 @@ public class Solution {
                 }
         );
 
-        try {
-            Scanner scanner = new Scanner(new FileReader(fileName));
-            while (scanner.hasNext()) {
-                set.add(scanner.next());
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            while (reader.ready()) {
+                String line = reader.readLine();
+                String[] str = line.trim().toLowerCase().split("[\\p{IsPunctuation}\\p{IsWhite_Space}\\n]+");
+                set.addAll(Arrays.asList(str));
             }
-            scanner.close();
             System.out.println(set);
         } catch (IOException e) {
             e.printStackTrace();
@@ -61,6 +65,42 @@ public class Solution {
 
     /**
      * Задание 3: Подсчитайте и выведите на экран сколько раз каждое слово встречается в файле.
+     */
+    public void wordCounterList(String fileName) {
+        Map<String, Integer> map = new HashMap<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            while (reader.ready()) {
+                String line = reader.readLine();
+                String[] S = line.trim().toLowerCase().split("[\\p{IsPunctuation}\\p{IsWhite_Space}\\n]+");
+                for (String word : S) {
+                    if (map.containsKey(word)) {
+                        map.replace(word, map.get(word) + 1);
+                    } else map.put(word, 1);
+                }
+            }
+            System.out.println(map);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Задание 4: Выведите на экран все строки файла в обратном порядке.
+     */
+    public void reverseLine(String fileName) {
+        List<String> list = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            while (reader.ready()) {
+                list.add(new StringBuilder(reader.readLine()).reverse().toString());
+            }
+            for (int i = list.size() - 1; i >= 0; i--) System.out.println(list.get(i));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Задание 5: Реализуйте свой Iterator для обхода списка в обратном порядке.
      */
 
 
