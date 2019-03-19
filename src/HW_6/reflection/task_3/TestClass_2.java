2ca1b730c85b19ba370f884d1e549ba475347bfa
ADN <1434-3541@mail.ru>
2019-03-20 01:15:23 +0300

    Commit HW6

--- src/HW_6/reflection/task_3/TestClass_2.java
+++ src/HW_6/reflection/task_3/TestClass_2.java
@@ -9,6 +9,10 @@ public class TestClass_2 extends TestClass {
         System.out.println(method_2.isAnnotationPresent(Cache.class));
 
         System.out.println(TestClass_2.class.isAnnotationPresent(MyAnnotation.class));
+
+        // Даже при типе интерфейса он смотри анннотации метода именно у класса (Однако это не касается Proxy - он смотрит аннотации методов интерфейса)
+        TestInterface test = new TestClass_2();
+        System.out.println(test.getClass().getMethod("method_2", int.class, int.class).isAnnotationPresent(Cache.class));
     }
 
 }
